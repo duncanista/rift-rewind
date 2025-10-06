8be9c57d784ba0afb94055b38dbe3757a6f49447
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-10-06 16:46:58 -0400

    connect integration from Lambda API

--- rewind-infra/lib/rewind-infra-stack.ts
+++ rewind-infra/lib/rewind-infra-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 const prefix = 'rift-rewind';
 
@@ -18,7 +20,8 @@ export class RewindInfraStack extends cdk.Stack {
     // Set up Amplify hosting
     this.setupAmplify();
 
-    // Future resources can be added here
+    // Set up Lambda and Secrets Manager
+    this.setupLambda();
   }
 
   private setupAmplify(): void {
@@ -63,6 +66,79 @@ export class RewindInfraStack extends cdk.Stack {
     new cdk.CfnOutput(this, `${prefix}-branch-url`, {
       value: `https://${mainBranch.branchName}.${AMPLIFY_APP_ID}.amplifyapp.com`,
       description: 'Amplify Branch URL',
+    });
+  }
+
+  private setupLambda(): void {
+    // Get Riot API key from environment variable
+    const riotApiKey = process.env.RIOT_API_KEY;
+    if (!riotApiKey) {
+      throw new Error('RIOT_API_KEY environment variable is not set');
+    }
+
+    // Create Secrets Manager secret for Riot API key
+    const riotApiKeySecret = new secretsmanager.Secret(this, `${prefix}-riot-api-key-${environment}`, {
+      secretName: `${prefix}/riot-api-key/${environment}`,
+      description: 'Riot API key for fetching League of Legends match data',
+      secretStringValue: cdk.SecretValue.unsafePlainText(riotApiKey),
+    });
+
+    // Create Lambda function for match data aggregation
+    // Bundle Python dependencies with the Lambda code
+    const aggregatorLambda = new lambda.Function(this, `${prefix}-aggregator-${environment}`, {
+      functionName: `${prefix}-aggregator-${environment}`,
+      runtime: lambda.Runtime.PYTHON_3_12,
+      handler: 'aggregator.handler.lambda_handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '..', '..', 'rewind-backend', 'lambdas'), {
+        bundling: {
+          image: lambda.Runtime.PYTHON_3_12.bundlingImage,
+          command: [
+            'bash', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
+          ],
+        },
+      }),
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 512,
+      environment: {
+        RIOT_API_KEY_SECRET_ARN: riotApiKeySecret.secretArn,
+      },
+      description: 'Aggregates League of Legends match data for a summoner',
+    });
+
+    // Grant Lambda permission to read the secret
+    riotApiKeySecret.grantRead(aggregatorLambda);
+
+    // Create Lambda Function URL with CORS
+    // Include localhost for development/testing
+    const allowedOrigins = environment === 'dev' 
+      ? ['https://riftrewind.lol', 'https://www.riftrewind.lol', 'http://localhost:3000', 'http://localhost:3001']
+      : ['https://riftrewind.lol', 'https://www.riftrewind.lol'];
+    
+    const functionUrl = aggregatorLambda.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins,
+        allowedMethods: [lambda.HttpMethod.POST],
+        allowedHeaders: ['Content-Type', 'X-Requested-With'],
+        maxAge: cdk.Duration.hours(1),
+      },
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, `${prefix}-lambda-function-name`, {
+      value: aggregatorLambda.functionName,
+      description: 'Aggregator Lambda Function Name',
+    });
+
+    new cdk.CfnOutput(this, `${prefix}-lambda-function-url`, {
+      value: functionUrl.url,
+      description: 'Aggregator Lambda Function URL',
+    });
+
+    new cdk.CfnOutput(this, `${prefix}-secret-arn`, {
+      value: riotApiKeySecret.secretArn,
+      description: 'Riot API Key Secret ARN',
     });
   }
 }
